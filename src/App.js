dedbcc4a133816a54f6684bf5b7022ed23a7a78f
pedro braz <braz.hpedro@gmail.com>
2020-06-04 19:27:00 -0300

    add signalr real time chart using rechart

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import {
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend
+} from 'recharts';
+import * as signalR from "@microsoft/signalr";
+
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const hubConnection = new signalR.HubConnectionBuilder()
+  .withUrl("https://localhost:5001/stream")
+  .configureLogging(signalR.LogLevel.Information)
+  .build();
+
+  useEffect(() => {
+    hubConnection.start().then(() => {
+      hubConnection.stream("Temperatures").subscribe({
+        next: (item) => {
+          console.log(item);
+          setData(currentData => [...currentData, item]);
+        },
+        complete: () => {
+          console.log('completo');
+        },
+        error: (err) => {
+          console.log(err);
+        }
+      })
+    });
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="chart">
+      <LineChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="tempo" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="temp1" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="temp2" stroke="#82ca9d" />
+      </LineChart>
     </div>
   );
 }
